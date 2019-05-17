41da82f0128f8e164a98880ece612199148c17d8
Yaroslav Melmychenko <yaroslav.melnychenko@chisw.com>
2019-05-17 13:18:55 +0300

    grammar page layout

--- src/pages/Test/Grammar/Grammar.jsx
+++ src/pages/Test/Grammar/Grammar.jsx
@@ -1,5 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import OneTask from '../components/OneTask';
+import './Grammar.scss';
 
-const Grammar = () => <div>Grammar</div>;
+class Grammar extends Component {
+  state = {
+    questions: [
+      {
+        id: 1,
+        title: 'Which one 1?',
+        options: [
+          'this',
+          'or this',
+          'no, this',
+          'no no no',
+        ],
+      },
+      {
+        id: 2,
+        title: 'Which one 2?',
+        options: [
+          'this or this no, this no no no',
+          'or this no, this',
+          'no, this',
+          'no no no no no no no no no',
+        ],
+      },
+      {
+        id: 3,
+        title: 'Which one 3?',
+        options: [
+          'this',
+          'or this',
+          'no, this',
+          'no no no',
+        ],
+      },
+      {
+        id: 4,
+        title: 'Which one 4?',
+        options: [
+          'this',
+          'or this',
+          'no, this',
+          'no no no',
+        ],
+      },
+      {
+        id: 5,
+        title: 'Which one 5?',
+        options: [
+          'this',
+          'or this',
+          'no, this',
+          'no no no',
+        ],
+      },
+    ],
+  }
+
+  submitTest = () => {
+    window.console.log('submitTest');
+  }
+
+  render() {
+    const { questions } = this.state;
+    return (
+      <div className="test-container">
+        <h2>Grammar tasks</h2>
+        {
+          questions.map((question, i) => <OneTask number={i + 1} key={question.id} {...question} />)
+        }
+        <div className="btn-container">
+          <Button variant="contained" color="primary" onClick={this.submitTest}>
+            Submit grammar answers
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Grammar;
