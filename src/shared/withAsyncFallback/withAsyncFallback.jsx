a390a0b9e20a96400d4887b0c1b4528503703976
Yaroslav Melmychenko <yaroslav.melnychenko@chisw.com>
2019-05-23 17:04:08 +0300

    add loader

--- src/shared/withAsyncFallback/withAsyncFallback.jsx
+++ src/shared/withAsyncFallback/withAsyncFallback.jsx
@@ -1,8 +1,10 @@
 import React, { Suspense } from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import './loading.scss';
 
 import { getDisplayName } from '../../utils';
 
-const withAsyncFallback = (Component, Loader = <h2>Loading...</h2>) => {
+const withAsyncFallback = (Component, Loader = <div className="loading"><CircularProgress /></div>) => {
   const WrappedComponent = () => (
     <Suspense fallback={Loader}>
       <Component />
